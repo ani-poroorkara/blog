397b160dc96dfe027df37dbefb8dece71ceeb812
ani-poroorkara <anirudh_p@outlook.com>
2022-07-01 15:24:30 -0400

    added blog routing

--- src/app/blog/blog-routing.module.ts
+++ src/app/blog/blog-routing.module.ts
@@ -1,22 +1,22 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BlogListComponent } from './blog-list/blog-list.component';
+import { BlogPostComponent } from './blog-post/blog-post.component';
 
-import {BlogComponent} from './blog.component';
+import { BlogComponent } from './blog.component';
 
 const routes: Routes = [
   {
-    path: ':slug',
+    path: '',
     component: BlogComponent,
+    children: [
+      { path: ':slug', component: BlogPostComponent },
+      { path: '', component: BlogListComponent },
+    ],
   },
-  {
-    path: '**',
-    component: BlogComponent,
-  }
 ];
-
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
 export class BlogRoutingModule {}
-
