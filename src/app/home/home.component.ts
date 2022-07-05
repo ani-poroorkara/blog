79e987232e39af2487e8ef8ea63c63befbabb16b
ani-poroorkara <anirudh_p@outlook.com>
2022-07-05 16:52:39 -0400

    added new post dates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { ScullyRoutesService, ScullyRoute } from '@scullyio/ng-lib';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  
+  constructor(private scully: ScullyRoutesService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    // debug current pages
+    this.scully.available$.subscribe(routes => console.log(routes));
   }
 
+  $blogPosts = this.scully.available$.pipe(
+    map((routes: ScullyRoute[]) =>  
+      routes.filter(
+        (route: ScullyRoute) =>
+        route.route.startsWith('/blog/') && route.sourceFile?.endsWith('.md')
+      )
+    ),
+    map((blogs) => blogs.sort((a, b) => (a.date < b.date ? 1 : -1)))
+  );
 }
